5ab28d0859ed451f89cde77373481fcc2aa1a080
ganes <ganes@Lenovo_Idepad>
2024-04-04 14:50:37 +0530

    Pushed

---
+++ src/winsoft_assignment/Abc.java
@@ -0,0 +1,32 @@
+package winsoft_assignment;
+
+import java.util.HashMap;
+
+public class Abc {
+    public static void main(String[] args) {
+        String str = "Betty bought a bit of butter, But the butter was so bitter, So she bought some better butter, To make the bitter butter better.";
+        
+        // Remove special characters and convert to lowercase
+        str = str.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+
+        // Split the string into words
+        String[] words = str.split("\\s+");
+
+        // Create a HashMap to store word counts
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+
+        // Count the occurrences of each word
+        for (String word : words) {
+            if (wordCountMap.containsKey(word)) {
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+                wordCountMap.put(word, 1);
+            }
+        }
+
+        // Print the word counts
+        for (String word : wordCountMap.keySet()) {
+            System.out.println(word + ": " + wordCountMap.get(word));
+        }
+    }
+}
---
+++ src/winsoft_assignment/Dup.java
@@ -0,0 +1,21 @@
+package winsoft_assignment;
+
+public class Dup {
+    public static void main(String[] args) {
+        String str = "Telecommunication";
+        int[] charCount = new int[256]; // Assuming ASCII characters
+
+        // Count the occurrences of each character
+        for (char c : str.toCharArray()) {
+            charCount[c]++;
+        }
+
+        // Print duplicate characters
+        System.out.println("Duplicate characters in the string '" + str + "':");
+        for (int i = 0; i < charCount.length; i++) {
+            if (charCount[i] > 1) {
+                System.out.println((char)i);
+            }
+        }
+    }
+}
